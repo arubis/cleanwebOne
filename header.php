d7b858717080589d8562a4a55f94ad535a80cd56
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-01-28 13:31:55 -0700

    Link to jQuery Mobile

--- header.php
+++ header.php
@@ -29,12 +29,14 @@
       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
-    <!-- Bootstrap and jQuery import -->
+    <!-- Bootstrap, jQuery, jQ UI, jQ Mobile import -->
     <!-- the main CSS is now pulled into our Sass... -->
     <!-- <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" type="text/css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
     <script src="//code.jquery.com/ui/1.10.4/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="//code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css" type="text/css">
+    <script src="//code.jquery.com/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
 
     <!-- get our custom CSS -->
