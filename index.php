ecad32d88fb89eea7f0f7431903fb1895c154233
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-01-31 14:20:57 -0700

    Clean up loops. Set up page template. Add HR logic.

--- index.php
+++ index.php
@@ -2,25 +2,41 @@
 
 <?php get_header(); ?>
 
+<?php 
+function more_posts() { // see if there are posts
+  global $wp_query;     // following the current one
+  return $wp_query->current_post + 1 < $wp_query->post_count;
+} ?>
+
   <main role="main">
 
-    <section class="posts">
+    <section class="posts" id="main-wrap">
 
       <?php // Loop it. ?>
       <?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
 
         <div class="row">
-          <div class="post col-sm-8 col-sm-offset-2">
+          <div class="post col-sm-8 col-sm-offset-2"><?php //Alas, would that this were semantic. Sass's @import doesn't play nice.?>
             <h2 class="title"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
-            <div class="entry">
+            <div class="content entry">
               <?php the_content(); ?>
             </div>
 
             <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
 
-
         </div>
+
+      <?php // put an HR after current post if there's
+      // another coming
+      if ( more_posts() ):  ?>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <hr />
+        </div>
+      </div>
+      <?php endif; ?>
+
 
       <?php endwhile; else: ?>
       <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
--- page.php
+++ page.php
@@ -3,28 +3,46 @@
 
 <?php get_header(); ?>
 
+<?php 
+function more_posts() { // see if there are posts
+  global $wp_query;     // following the current one
+  return $wp_query->current_post + 1 < $wp_query->post_count;
+} ?>
+
 <main role="main">
-  <section id="main-wrap">
-    <?php while ( have_posts() ) : the_post(); ?>
-      <div class="page container">
+  <section class="pages" id="main-wrap">
 
-        <div class="row">
-          <div class="page title content">
-            <h1 id="post-title"><?php the_title(); ?></h1>
-          </div>
-        </div><!-- /row -->
-          
-          <?php  $layoutType=get_post_meta(get_the_id(), 'layout-type', true); ?>
+    <?php // Loop it. ?>
+    <?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
 
-        <div class="row">                
-          <div class="page content">
-            <?php the_content(''); ?>
+      <div class="row">
+        <div class="page col-sm-8 col-sm-offset-2"><?php //Alas, would that this were semantic. Sass's @import doesn't play nice.?>
+          <h2 class="title"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+
+          <div class="entry content">
+            <?php the_content(); ?>
           </div>
-        </div><!-- /row -->
-        
-      </div><!-- /page container -->
-    <?php endwhile; ?>    
+
+          <?php // don't really need author/ts for pages ?>
+        </div>
+      </div>
+
+      <?php // put an HR after current post if there's
+            // another coming
+      if ( more_posts() ):  ?>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+          <hr />
+        </div>
+      </div>
+      <?php endif; ?>
+
+    <?php endwhile; else: ?>
+      <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+
   </section>
+
 </main>
 
 <?php get_footer(); ?>
